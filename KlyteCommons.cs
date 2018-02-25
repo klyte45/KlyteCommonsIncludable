1ae34640ea0832fdd545303a0d128046a33911f8
Leandro <klyte45@hotmail.com>
2018-02-25 00:19:55 -0300

    Versão 1.0

---
+++ KlyteCommons.cs
@@ -0,0 +1,97 @@
+using ColossalFramework;
+using ColossalFramework.UI;
+using ICities;
+using Klyte.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using ColossalFramework.DataBinding;
+using ColossalFramework.Globalization;
+using Klyte.Commons.Utils;
+using Klyte.Commons.Extensors;
+using Klyte.Commons.Overrides;
+using ColossalFramework.PlatformServices;
+
+[assembly: AssemblyVersion("1.0.0.*")]
+namespace Klyte.Commons
+{
+    public class KlyteCommonsMod : IUserMod, ILoadingExtension
+    {
+
+        public string Name => "Klyte Commons " + KlyteCommonsMod.version;
+        public string Description => "Base mod for Klyte45 mods. Required dependency";
+
+        public void OnCreated(ILoading loading) { }
+
+        public void OnLevelLoaded(LoadMode mode)
+        {
+            if (mode != LoadMode.LoadGame && mode != LoadMode.NewGame && mode != LoadMode.NewGameFromScenario)
+            {
+                return;
+            }
+
+            GameObject gameObject = new GameObject
+            {
+                name = "KlyteCommons"
+            };
+
+            var typeTarg = typeof(Redirector<>);
+            var instances = from t in Assembly.GetAssembly(typeof(KlyteCommonsMod)).GetTypes()
+                            let y = t.BaseType
+                            where t.IsClass && !t.IsAbstract && y != null && y.IsGenericType && y.GetGenericTypeDefinition() == typeTarg
+                            select t;
+
+            foreach (Type t in instances)
+            {
+                gameObject.AddComponent(t);
+            }
+
+        }
+
+        public void OnLevelUnloading()
+        {
+        }
+
+        public void OnReleased()
+        {
+        }
+
+        public static string minorVersion
+        {
+            get {
+                return majorVersion + "." + typeof(KlyteCommonsMod).Assembly.GetName().Version.Build;
+            }
+        }
+        public static string majorVersion
+        {
+            get {
+                return typeof(KlyteCommonsMod).Assembly.GetName().Version.Major + "." + typeof(KlyteCommonsMod).Assembly.GetName().Version.Minor;
+            }
+        }
+        public static string fullVersion
+        {
+            get {
+                return minorVersion + " r" + typeof(KlyteCommonsMod).Assembly.GetName().Version.Revision;
+            }
+        }
+        public static string version
+        {
+            get {
+                if (typeof(KlyteCommonsMod).Assembly.GetName().Version.Minor == 0 && typeof(KlyteCommonsMod).Assembly.GetName().Version.Build == 0)
+                {
+                    return typeof(KlyteCommonsMod).Assembly.GetName().Version.Major.ToString();
+                }
+                if (typeof(KlyteCommonsMod).Assembly.GetName().Version.Build > 0)
+                {
+                    return minorVersion;
+                }
+                else
+                {
+                    return majorVersion;
+                }
+            }
+        }
+    }
+}
---
+++ Overrides/DistrictManagerOverrides.cs
@@ -0,0 +1,51 @@
+﻿using ColossalFramework;
+using ColossalFramework.Globalization;
+using ColossalFramework.UI;
+using ICities;
+using Klyte.Extensions;
+using Klyte.Harmony;
+using Klyte.Commons.Extensors;
+using Klyte.Commons.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+
+namespace Klyte.Commons.Overrides
+{
+    public class DistrictManagerOverrides : Redirector<DistrictManagerOverrides>
+    {
+
+
+        #region Events
+        public delegate void OnDistrictChanged();
+        public static event OnDistrictChanged eventOnDistrictRenamed;
+
+        private static bool OnDistrictRenamed()
+        {
+            eventOnDistrictRenamed?.Invoke();
+            return true;
+        }
+        #endregion
+
+        #region Hooking
+
+        public override void Awake()
+        {
+            KlyteUtils.doLog("Loading District Manager Overrides");
+            #region Release Line Hooks
+            MethodInfo preRename = typeof(DistrictManagerOverrides).GetMethod("OnDistrictRenamed", allFlags);
+
+            AddRedirect(typeof(DistrictManager).GetMethod("UpdateNames", allFlags), preRename);
+            #endregion
+
+
+        }
+        #endregion
+
+
+
+    }
+}
