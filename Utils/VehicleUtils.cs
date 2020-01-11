5e36af235d4052b49efa14f719523a7cbdb11412
Leandro Klyte <klyte45@hotmail.com>
2020-01-11 01:43:31 -0300

    More vehicles utility methods

--- Utils/VehicleUtils.cs
+++ Utils/VehicleUtils.cs
@@ -1,10 +1,14 @@
-﻿using ColossalFramework.Globalization;
+﻿using ColossalFramework;
+using ColossalFramework.Globalization;
 using ColossalFramework.Math;
+using ColossalFramework.Threading;
 using Klyte.Commons.Extensors;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using UnityEngine;
 
 namespace Klyte.Commons.Utils
 {
@@ -157,6 +161,122 @@ namespace Klyte.Commons.Utils
             instance.m_vehicles.m_buffer[idx].Info = newInfo;
             instance.m_vehicles.m_buffer[idx].Spawn(idx);
             newInfo.m_vehicleAI.CreateVehicle(idx, ref instance.m_vehicles.m_buffer[idx]);
+        }
+
+        private static int GetUnitsCapacity(VehicleAI vehicleAI)
+        {
+            VehicleAI vehicleAI2 = vehicleAI as AmbulanceAI;
+            if (vehicleAI2 != null)
+            {
+                return ((AmbulanceAI) vehicleAI2).m_patientCapacity + ((AmbulanceAI) vehicleAI2).m_paramedicCount;
+            }
+            vehicleAI2 = (vehicleAI as BusAI);
+            if (vehicleAI2 != null)
+            {
+                return ((BusAI) vehicleAI2).m_passengerCapacity;
+            }
+            vehicleAI2 = (vehicleAI as HearseAI);
+            if (vehicleAI2 != null)
+            {
+                return ((HearseAI) vehicleAI2).m_corpseCapacity + ((HearseAI) vehicleAI2).m_driverCount;
+            }
+            vehicleAI2 = (vehicleAI as PassengerPlaneAI);
+            if (vehicleAI2 != null)
+            {
+                return ((PassengerPlaneAI) vehicleAI2).m_passengerCapacity;
+            }
+            vehicleAI2 = (vehicleAI as PassengerShipAI);
+            if (vehicleAI2 != null)
+            {
+                return ((PassengerShipAI) vehicleAI2).m_passengerCapacity;
+            }
+            vehicleAI2 = (vehicleAI as PassengerTrainAI);
+            if (vehicleAI2 != null)
+            {
+                return ((PassengerTrainAI) vehicleAI2).m_passengerCapacity;
+            }
+            vehicleAI2 = (vehicleAI as TramAI);
+            if (vehicleAI2 != null)
+            {
+                return ((TramAI) vehicleAI2).m_passengerCapacity;
+            }
+            vehicleAI2 = (vehicleAI as CableCarAI);
+            if (vehicleAI2 != null)
+            {
+                return ((CableCarAI) vehicleAI2).m_passengerCapacity;
+            }
+            vehicleAI2 = (vehicleAI as PassengerFerryAI);
+            if (vehicleAI2 != null)
+            {
+                return ((PassengerFerryAI) vehicleAI2).m_passengerCapacity;
+            }
+            vehicleAI2 = (vehicleAI as PassengerBlimpAI);
+            if (vehicleAI2 != null)
+            {
+                return ((PassengerBlimpAI) vehicleAI2).m_passengerCapacity;
+            }
+            return -1;
+        }
+
+        private static int GetTotalUnitGroups(uint unitID)
+        {
+            int num = 0;
+            while (unitID != 0u)
+            {
+                CitizenUnit citizenUnit = Singleton<CitizenManager>.instance.m_units.m_buffer[(int) ((UIntPtr) unitID)];
+                unitID = citizenUnit.m_nextUnit;
+                num++;
+            }
+            return num;
+        }
+
+        public static IEnumerator UpdateCapacityUnits(ThreadBase t)
+        {
+            int count = 0;
+            Array16<Vehicle> vehicles = Singleton<VehicleManager>.instance.m_vehicles;
+            int i = 0;
+            while ((long) i < (long) ((ulong) vehicles.m_size))
+            {
+                if ((vehicles.m_buffer[i].m_flags & Vehicle.Flags.Spawned) == Vehicle.Flags.Spawned )
+                {
+                    int capacity = GetUnitsCapacity(vehicles.m_buffer[i].Info.m_vehicleAI);
+                    if (capacity != -1)
+                    {
+                        CitizenUnit[] units = Singleton<CitizenManager>.instance.m_units.m_buffer;
+                        uint unit = vehicles.m_buffer[i].m_citizenUnits;
+                        int currentUnitCount = GetTotalUnitGroups(unit);
+                        int newUnitCount = Mathf.CeilToInt((float) capacity / 5f);
+                        if (newUnitCount < currentUnitCount)
+                        {
+                            uint j = unit;
+                            for (int k = 1; k < newUnitCount; k++)
+                            {
+                                j = units[(int) ((UIntPtr) j)].m_nextUnit;
+                            }
+                            Singleton<CitizenManager>.instance.ReleaseUnits(units[(int) ((UIntPtr) j)].m_nextUnit);
+                            units[(int) ((UIntPtr) j)].m_nextUnit = 0u;
+                            count++;
+                        }
+                        else if (newUnitCount > currentUnitCount)
+                        {
+                            uint l = unit;
+                            while (units[(int) ((UIntPtr) l)].m_nextUnit != 0u)
+                            {
+                                l = units[(int) ((UIntPtr) l)].m_nextUnit;
+                            }
+                            int newCapacity = capacity - currentUnitCount * 5;
+                            Singleton<CitizenManager>.instance.CreateUnits(out units[(int) ((UIntPtr) l)].m_nextUnit, ref Singleton<SimulationManager>.instance.m_randomizer, 0, (ushort) i, 0, 0, 0, newCapacity, 0);
+                            count++;
+                        }
+                    }
+                }
+                if (i % 256 == 255)
+                {
+                    yield return null;
+                }
+                i++;
+            }
+            yield break;
         }
 
         #endregion
