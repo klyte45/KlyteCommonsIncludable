c32e455bb6a8010f136b1d913c1a5a8256f2c0f1
Leandro Klyte <klyte45@hotmail.com>
2020-06-15 21:47:35 -0300

    Added current mod icon as default when serializing data to K45Dialog

--- UI/K45DialogControl.cs
+++ UI/K45DialogControl.cs
@@ -18,7 +18,7 @@ namespace Klyte.Commons.Utils
     internal class K45DialogControl : UICustomControl
     {
         public const string PANEL_ID = "K45Dialog";
-        public const string VERSION = "20200514";
+        public const string VERSION = "20200615";
         private const string TEXT_INPUT_ID = "TextInput";
         private const string DD_INPUT_ID = "DropDownInput";
         private const string TUTORIAL_FOLDER_NAME = "Tutorial";
@@ -765,7 +765,7 @@ namespace Klyte.Commons.Utils
                 return new Dictionary<string, object>()
                 {
                     ["title"] = title,
-                    ["icon"] = icon,
+                    ["icon"] = icon ?? CommonProperties.ModIcon,
                     ["showClose"] = showClose,
                     ["message"] = message,
                     ["messageAlign"] = messageAlign,
