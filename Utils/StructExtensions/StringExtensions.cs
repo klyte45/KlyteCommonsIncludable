e62875b6dd872937963ac9af7eab4deb70cb11b6
Leandro Klyte <klyte45@hotmail.com>
2020-06-21 16:03:03 -0300

    Added string extensions

---
+++ Utils/StructExtensions/StringExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Klyte.Commons.Utils
+{
+    public static class StringExtensions
+    {
+        public static string Right(this string original, int numberCharacters) => original.Substring(original.Length - numberCharacters);
+    }
+}
