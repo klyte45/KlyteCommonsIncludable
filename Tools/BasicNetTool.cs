00d840608a8af7d471500e516af4b37e959de2fa
Leandro Klyte <klyte45@hotmail.com>
2021-10-15 13:33:42 -0300

    Fixing issues in the net tool NetAIWrapper

--- Tools/BasicNetTool.cs
+++ Tools/BasicNetTool.cs
@@ -165,7 +165,7 @@ namespace Klyte.Commons
             output.m_disaster = 0;
             output.m_currentEditObject = false;
             var ray = new Segment3(origin, vector);
-            RayCastSegmentAndNode(input.m_buildObject as NetInfo, ray, input.m_netSnap, input.m_segmentNameOnly, input.m_netService.m_service, input.m_netService2.m_service, input.m_netService.m_subService, input.m_netService2.m_subService, input.m_netService.m_itemLayers, input.m_netService2.m_itemLayers, input.m_ignoreNodeFlags, input.m_ignoreSegmentFlags, out _, out output.m_netNode, out output.m_netSegment);
+            RayCastSegmentAndNode(input.m_buildObject as NetInfo, ray, input.m_netSnap, input.m_segmentNameOnly, input.m_netService.m_service, input.m_netService2.m_service, input.m_netService.m_subService, input.m_netService2.m_subService, input.m_netService.m_itemLayers, input.m_netService2.m_itemLayers, input.m_ignoreNodeFlags, input.m_ignoreSegmentFlags, out m_raycastHit, out output.m_netNode, out output.m_netSegment);
 
             m_hoverSegment = output.m_netSegment;
 
@@ -191,8 +191,8 @@ namespace Klyte.Commons
             return num / (Stopwatch.Frequency / 1000L);
         }
 
-        protected static NetSegment[] SegmentBuffer => Singleton<NetManager>.instance.m_segments.m_buffer;
-        protected static NetNode[] NodeBuffer => Singleton<NetManager>.instance.m_nodes.m_buffer;
+        protected static ref NetSegment[] SegmentBuffer => ref Singleton<NetManager>.instance.m_segments.m_buffer;
+        protected static ref NetNode[] NodeBuffer => ref Singleton<NetManager>.instance.m_nodes.m_buffer;
 
 
 
@@ -210,6 +210,8 @@ namespace Klyte.Commons
         public static Shader shaderSolid = Shader.Find("Custom/Props/Decal/Solid");
 
         protected ushort m_hoverSegment;
+
+        protected Vector3 m_raycastHit;
 
         private bool m_prevRenderZones;
 
---
+++ Utils/UtilitiesClasses/NetAIWrapper.cs
@@ -0,0 +1,170 @@
+﻿using System.Reflection;
+
+namespace Klyte.Commons.Utils
+{
+    public class NetAIWrapper
+    {
+        public NetAI AI { get; }
+        private FieldInfo m_elevated;
+        private FieldInfo m_bridge;
+        private FieldInfo m_slope;
+        private FieldInfo m_tunnel;
+        private FieldInfo m_invisible;
+
+        public NetAIWrapper(NetAI ai)
+        {
+            AI = ai;
+
+            try
+            {
+                m_elevated = AI.GetType().GetField("m_elevatedInfo");
+                m_bridge = AI.GetType().GetField("m_bridgeInfo");
+                m_slope = AI.GetType().GetField("m_slopeInfo");
+                m_tunnel = AI.GetType().GetField("m_tunnelInfo");
+                m_invisible = AI.GetType().GetField("m_invisible");
+            }
+            catch
+            {
+                m_elevated = null;
+                m_bridge = null;
+                m_slope = null;
+                m_tunnel = null;
+                m_invisible = null;
+            }
+        }
+
+        public bool HasElevation => m_elevated != null && m_bridge != null && m_slope != null && m_tunnel != null;
+
+        public NetInfo Default
+        {
+            get => AI.m_info;
+            set => AI.m_info = value;
+        }
+
+        public NetInfo Elevated
+        {
+            get => HasElevation ? m_elevated.GetValue(AI) as NetInfo : null;
+            set
+            {
+                if (!HasElevation)
+                {
+                    return;
+                }
+
+                m_elevated.SetValue(AI, value);
+            }
+        }
+
+        public NetInfo Bridge
+        {
+            get => HasElevation ? m_bridge.GetValue(AI) as NetInfo : null;
+            set
+            {
+                if (!HasElevation)
+                {
+                    return;
+                }
+
+                m_bridge.SetValue(AI, value);
+            }
+        }
+
+        public NetInfo Slope
+        {
+            get => HasElevation ? m_slope.GetValue(AI) as NetInfo : null;
+            set
+            {
+                if (!HasElevation)
+                {
+                    return;
+                }
+
+                m_slope.SetValue(AI, value);
+            }
+        }
+
+        public NetInfo Tunnel
+        {
+            get => HasElevation ? m_tunnel.GetValue(AI) as NetInfo : null;
+            set
+            {
+                if (!HasElevation)
+                {
+                    return;
+                }
+
+                m_tunnel.SetValue(AI, value);
+            }
+        }
+
+
+        public bool IsInvisible() => m_invisible != null ? (bool)m_invisible.GetValue(AI) : false;
+
+
+        public enum ElevationType
+        {
+            None = -1,
+            Default,
+            Tunnel,
+            Ground,
+            Elevated,
+            Bridge,
+            Slope
+        }
+
+        internal NetInfo RelativeTo(ElevationType elevationType)
+        {
+            if (!HasElevation)
+            {
+                return Default;
+            }
+
+            switch (elevationType)
+            {
+                default:
+                case ElevationType.None:
+                case ElevationType.Default:
+                case ElevationType.Ground:
+                    return Default;
+                case ElevationType.Tunnel:
+                    return Tunnel ?? Default;
+                case ElevationType.Elevated:
+                    return Elevated ?? Default;
+                case ElevationType.Bridge:
+                    return Bridge ?? Default;
+                case ElevationType.Slope:
+                    return Slope ?? Default;
+            }
+        }
+
+        internal ElevationType ToType(NetInfo oldInfo)
+        {
+            if (!HasElevation)
+            {
+                return AI.IsUnderground() ? ElevationType.Tunnel : Default.m_clipTerrain ? ElevationType.Ground : ElevationType.Elevated;
+            }
+
+            if (oldInfo == Default)
+            {
+                return ElevationType.Ground;
+            }
+            if (oldInfo == Tunnel)
+            {
+                return ElevationType.Tunnel;
+            }
+            if (oldInfo == Elevated)
+            {
+                return ElevationType.Elevated;
+            }
+            if (oldInfo == Bridge)
+            {
+                return ElevationType.Bridge;
+            }
+            if (oldInfo == Slope)
+            {
+                return ElevationType.Slope;
+            }
+            return ElevationType.None;
+        }
+    }
+}
