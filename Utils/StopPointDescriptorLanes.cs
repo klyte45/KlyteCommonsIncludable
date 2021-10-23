9e253aa2ac0fe05f955cae9c39fdb6a12228a3f7
Leandro Klyte <klyte45@hotmail.com>
2021-10-23 11:59:34 -0300

    Added platform lane field

--- Utils/StopPointDescriptorLanes.cs
+++ Utils/StopPointDescriptorLanes.cs
@@ -11,6 +11,9 @@ namespace Klyte.Commons.Utils
         public uint laneId;
         public sbyte subbuildingId;
         public Vector3 directionPath;
+        public uint platformLaneId;
+
+        public long UniquePlatformId => ((platformLaneId & 0x7FFFFFFF) << 31) | (laneId & 0x7FFFFFFF);
 
         public override string ToString() => $"{platformLine.Position(0.5f)} (w={width} | {vehicleType} | {subbuildingId} | {laneId} | DIR = {directionPath} ({directionPath.GetAngleXZ()}°))";
     }
