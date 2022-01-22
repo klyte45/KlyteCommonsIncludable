171a02615c55a9d1db3114035d5b794f365f7177
Leandro Klyte <klyte45@hotmail.com>
2022-01-22 10:40:13 -0300

    Removing bugged GUIColorPicker class

--- LiteUI/BaseElements/GUIRootWindowBase.cs
+++ LiteUI/BaseElements/GUIRootWindowBase.cs
@@ -4,7 +4,6 @@ namespace Klyte.Commons.LiteUI
 {
     public abstract class GUIRootWindowBase : GUIWindow
     {
-        internal abstract GUIColorPicker ColorPicker { get; }
         protected GUIRootWindowBase(string title, Rect rect, bool resizable = true, bool hasTitlebar = true, Vector2 minSize = default) : base(title, rect, resizable, hasTitlebar, minSize)
         {
 
